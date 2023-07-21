2be63d28134cda74befb2107bc9103e46a35756f
Wesley Kater <wesley@codecapi.nl>
2023-07-21 23:14:16 +0200

    Remaining config set up

---
+++ styles/fonts/index.ts
@@ -0,0 +1,11 @@
+import localFont from "next/font/local";
+
+const SpaceGroteskMedium = localFont({
+  src: "../../styles/fonts/SpaceGrotesk/SpaceGrotesk-Medium.woff2",
+});
+
+const NeueMontreal = localFont({
+  src: "../../styles/fonts/NeueMontreal/NeueMontreal-Medium.otf",
+});
+
+export { SpaceGroteskMedium, NeueMontreal };
